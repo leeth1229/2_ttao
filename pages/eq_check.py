7140a5d62846b7c794637ae4ecb1232ccef7596e
2_ttao <106638850+leeth1229@users.noreply.github.com>
2024-02-23 17:49:11 +0900

    Update eq_check.py

--- pages/eq_check.py
+++ pages/eq_check.py
@@ -1,94 +1,97 @@
 import pandas as pd
 from PIL import Image
 import streamlit as st
-from streamlit_drawable_canvas import st_canvas
+import streamlit_antd_components as sac
 import fitz  # PyMuPDF
-import io
+import re
+import os
 
 st.set_page_config(layout="wide")
 
-# PDF 파일 경로
-pdf_path = "streamlit/doc/pdf/test.pdf"  # PDF 파일 경로
-# PDF 문서 열기
-doc = fitz.open(pdf_path)
-
-
-
 # 초기 세션 상태 설정
 if 'page_number' not in st.session_state:
     st.session_state.page_number = 0
     st.session_state.rotation = 0  # 페이지 회전 상태 초기화
-    st.session_state.canvas_data = {}  # 페이지별 캔버스 데이터 저장
+
+# Define the root directory
+file_path = 'streamlit/docs/items'
+
+# Define a function to convert the directory structure to a tree structure
+def convert_dir_to_tree(file_path):
+    tree_items = []  # This will store the converted tree items.
+    for level_1 in os.listdir(file_path):
+        # Check if the item is a directory
+        if os.path.isdir(os.path.join(file_path, level_1)):
+            # Create a new TreeItem for the first level
+            item_1 = sac.TreeItem(level_1, disabled=True, tag=[sac.Tag('Plant', color='red')],children=[]) # 공장명
+            for level_2 in os.listdir(os.path.join(file_path, level_1)):
+                # Check if the item is a directory
+                if os.path.isdir(os.path.join(file_path, level_1, level_2)):
+                    # Create a new TreeItem for the second level
+                    item_2 = sac.TreeItem(level_2,icon='table', children=[]) # 설비명
+                    for level_3 in os.listdir(os.path.join(file_path, level_1, level_2)):
+                        # Check if the item is a directory
+                        if os.path.isdir(os.path.join(file_path, level_1, level_2, level_3)):
+                            # Create a new TreeItem for the third level
+                            item_3 = sac.TreeItem(level_3, icon='table',children=[]) # 추가 폴더
+                            item_2.children.append(item_3)
+                    item_1.children.append(item_2)
+            tree_items.append(item_1)
+    return tree_items
+
+# Convert the directory structure to a tree structure
+tree_items = convert_dir_to_tree(file_path)
 
 with st.sidebar:
-    st.write("## Page Rotation")
-    if st.button("Rotate 90°"):
-        st.session_state.rotation = (st.session_state.rotation + 90) % 360
-    if st.button("Rotate -90°"):
-        st.session_state.rotation = (st.session_state.rotation - 90) % 360
+    # Use the tree_items in your sac.tree
+    sac.divider(label='Tag Trees', align='center', color='gray')
+    
+    Item_ = sac.tree(
+        items=tree_items,
+        label='Tag No.',
+        size='sm',
+        color='#4682b4',
+        open_all=True,
+        checkbox=False,
+        return_index=True
+        )
+    
+    # 공장 폴더별로 csv 파일 생성 및 경로 추적
+    Item_path = []
+    for level1 in os.listdir(file_path): # 공장명
+        folder_path_1 = os.path.join(file_path + "/" + level1) # 공장 경로
+        Item_path.append(folder_path_1) # 경로 추적
+        
+        for level2 in os.listdir(folder_path_1): # 설비명
+            folder_path_2 = os.path.join(folder_path_1 + "/" + level2) # 설비 경로
+            if os.path.isdir(folder_path_1): # 폴더 추가 여부 확인
+                Item_path.append(folder_path_2) # 경로 추적
+                boom_path = os.path.join(folder_path_2 + "/" + f'{level2}.csv') # csv 파일 만들기
+                if not os.path.exists(boom_path):
+                    with open(boom_path, 'w') as f:
+                        f.write('Material Code, tag_no, dwg_no ,item_no, part_no/n')
 
-# 페이지 이동 버튼
-col1, col2 = st.columns(2)
-with col1:
-    if st.button('Previous Page'):
-        st.session_state.page_number = max(0, st.session_state.page_number - 1)
-with col2:
-    if st.button('Next Page'):
-        st.session_state.page_number = min(len(doc) - 1, st.session_state.page_number + 1)
+            for level3 in os.listdir(folder_path_2): # sub 폴더
+                folder_path_3 = os.path.join(folder_path_2 + "/" + level3) # 설비 경로
+                if os.path.isdir(folder_path_3): # 폴더 추가 여부 확인
+                    Item_path.append(folder_path_3) # 경로 추적
+                    boom_path_ = os.path.join(folder_path_3 + "/" + f'{level3}.csv') # csv 파일 만들기
+                    if not os.path.exists(boom_path_):
+                        with open(boom_path_, 'w') as f:
+                            f.write('something\n')
+                
+                    
+    st.write(Item_)
+    st.write(Item_path)
+    sac.divider(label='Code Match', align='center', color='gray')
 
-# 현재 페이지 로드 및 표시
-current_page_number = st.session_state.page_number
-scaling_matrix = fitz.Matrix(2.0, 2.0)  # 너비와 높이를 2배로 확장
-rotation_matrix = scaling_matrix.prerotate(st.session_state.rotation)
-
-# 현재 페이지의 PDF를 이미지로 변환
-current_page = doc[current_page_number]  # 현재 페이지 선택
-pix = current_page.get_pixmap(matrix=rotation_matrix)  # 회전 적용
-img = Image.open(io.BytesIO(pix.tobytes("png")))  # PDF 페이지를 이미지로 변환
-
-st.write(f"Page {current_page_number + 1}")  # 페이지 번호를 보여주기
-
-# 초기 세션 상태 설정
-if 'page_number' not in st.session_state:
-    st.session_state.page_number = 0
-    st.session_state.rotation = 0  # 페이지 회전 상태 초기화
-
-# 여기에 canvas_data 초기화 코드 추가
-if 'canvas_data' not in st.session_state:
-    st.session_state.canvas_data = {}  # 페이지별 캔버스 데이터 저장 초기화
+if Item_ is None:
+    st.image("streamlit/docs/items/OXO/OX-GB-101/회전설비.png")
+else: 
+    file_name = Item_path[Item_].split("/")[-1]
+    df_boom_list_path = Item_path[Item_] + "/" + f"{file_name}.csv"
+    st.sidebar.write(df_boom_list_path)
+    df_boom_list = pd.read_csv(df_boom_list_path)
+    st.data_editor(df_boom_list, num_rows="dynamic", use_container_width=True)
 
 
-# Specify canvas parameters in application
-drawing_mode = st.sidebar.selectbox("Drawing tool:", ["rect"])
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_color = st.sidebar.color_picker("Background color hex: ", "#ffffff")
-realtime_update = st.sidebar.checkbox("Update in realtime", True)
-
-# Load existing drawing for the current page or use a new canvas
-initial_data = st.session_state.canvas_data.get(current_page_number)
-
-# Create a canvas component with background image from current PDF page
-canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color=bg_color,
-    background_image=img,
-    update_streamlit=True,
-    width=pix.width,
-    height=pix.height,
-    drawing_mode=drawing_mode,
-    initial_drawing=initial_data,
-    key=f"canvas_{current_page_number}",
-)
-
-# Save the current drawing to session state
-st.session_state.canvas_data[current_page_number] = canvas_result.json_data
-
-# Display drawing data if available
-if canvas_result.json_data is not None:
-    objects = pd.json_normalize(canvas_result.json_data["objects"])
-    for col in objects.select_dtypes(include=['object']).columns:
-        objects[col] = objects[col].astype("str")
-    st.dataframe(objects)  # 데이터 프레임으로 변경
