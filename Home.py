d0d0e4093a34ea85628214bb497382566f284245
2_ttao <106638850+leeth1229@users.noreply.github.com>
2024-02-29 18:00:29 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,60 +6,59 @@ st.set_page_config(
     page_icon="👋",
 )
 
-# 로그인 성공 여부를 저장할 세션 상태 초기화
-if 'login_success' not in st.session_state:
-    st.session_state['login_success'] = False
-
-
-st.sidebar.success("Select a Tab above.")
+st.session_state.name = "billy"
+st.session_state.email = "none_nono_@lgchem.com"
 
 st.write("# Welcome👋")
 
-if st.session_state['login_success']:
-    st.sidebar.markdown("## Pages")
+# if st.session_state['Save_success']:
+#     st.sidebar.markdown("your keys")
 
 # Create an empty container
 placeholder = st.empty()
 
-actual_email = "email"
-actual_password = "password"
+# 로그인 성공 여부를 저장할 세션 상태 초기화
 
 # Insert a form in the container
 with placeholder.form("login"):
-    st.markdown("#### Enter your credentials")
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
-    submit = st.form_submit_button("Login")
+    st.markdown("#### Enter your information")
+    name = st.text_input("name")
+    email = st.text_input("email")
+    submit = st.form_submit_button("Save")
 
-if submit and email == actual_email and password == actual_password:
-    # If the form is submitted and the email and password are correct,
+if submit: # and name == actual_name and idNo == actual_idNo and email == actual_email
+    # If the form is submitted and the name and idNo are correct,
     # clear the form/container and display a success message
     placeholder.empty()
-    st.success("Login successful")
-    st.session_state['login_success'] = True
-    st.experimental_rerun()  # 페이지 새로고침
-elif submit and email != actual_email and password != actual_password:
-    st.error("Login failed")
+    st.success("Save successful")
+    st.session_state['Save_success'] = True
+    # st.experimental_rerun()  # 페이지 새로고침
+    st.session_state.name =name
+    st.session_state.email =email
+    
 else:
     pass
 
+st.sidebar.write("Name :",st.session_state.name)
+st.sidebar.write("Email :",st.session_state.email)
+
+
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
+    메일 주소 및 이름을 입력 시, 자재코드 다운로드 양식에 자동 반영 됩니다.(반영예정)
+    
+    👈 미 입력 시, 해당 Name & Email 로 자동 입력됩니다.
+    """
 )
+
+
+    # ### Want to learn more?
+    # - Check out [streamlit.io](https://streamlit.io)
+    # - Jump into our [documentation](https://docs.streamlit.io)
+    # - Ask a question in our [community
+    #     forums](https://discuss.streamlit.io)
+
+    # ### See more complex demos
+    # - Use a neural net to [analyze the Udacity Self-driving Car Image
+    #     Dataset](https://github.com/streamlit/demo-self-driving)
+    # - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
